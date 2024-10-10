5ef0bca196139794b9d192d8cc29bec508d17ca4
thbrbz <thbrbz@gmail.com>
2024-10-09 21:26:05 -0300

    Feat: New class Fighter added.

---
+++ src/main/java/org/example/exercises/Fighter.java
@@ -0,0 +1,51 @@
+package org.example.exercises;
+
+class Fighter {
+    boolean isVulnerable() {
+        return true;
+    }
+
+    int getDamagePoints(Fighter fighter) {
+        return 1;
+    }
+}
+
+class Warrior extends Fighter {
+    @Override
+    public String toString() {
+        return "Fighter is a Warrior";
+    }
+
+    @Override
+    public boolean isVulnerable() {
+        return false;
+    }
+
+    @Override
+    public int getDamagePoints(Fighter fighter) {
+        return fighter.isVulnerable() ? 10 : 6;
+    }
+}
+
+class Wizard extends Fighter {
+    boolean isSpellPrepared = false;
+
+    @Override
+    public String toString() {
+        return "Fighter is a Wizard";
+    }
+
+    public void prepareSpell() {
+        this.isSpellPrepared = true;
+    }
+
+    @Override
+    public boolean isVulnerable() {
+        return !this.isSpellPrepared;
+    }
+
+    @Override
+    public int getDamagePoints(Fighter fighter) {
+        return this.isSpellPrepared ? 12 : 3;
+    }
+}
