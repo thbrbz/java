0a7c57a5872287685dd260e0e1dd398e666cf7ca
thbrbz <thbrbz@gmail.com>
2024-10-18 23:55:53 -0300

    Feat: New exercise ArmstrongNumbers added.

---
+++ src/main/java/org/example/exercises/ArmstrongNumbers.java
@@ -0,0 +1,14 @@
+package org.example.exercises;
+
+class ArmstrongNumbers {
+
+    boolean isArmstrongNumber(int numberToCheck) {
+
+        String digits = String.valueOf(numberToCheck);
+        return numberToCheck == digits.chars()
+                .parallel()
+                .map(c -> (int) Math.pow(Character.getNumericValue(c), digits.length()))
+                .sum();
+    }
+
+}
