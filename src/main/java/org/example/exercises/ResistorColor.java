5ebbfd735f69ad837cf390be03a97519c899f5f9
thbrbz <thbrbz@gmail.com>
2024-10-31 22:27:08 -0300

    Feat (ResistorColor): Added new exercise.

---
+++ src/main/java/org/example/exercises/ResistorColor.java
@@ -0,0 +1,14 @@
+package org.example.exercises;
+
+import java.util.Arrays;
+
+class ResistorColor {
+
+    int colorCode(String color) {
+        return Arrays.asList(colors()).indexOf(color);
+    }
+
+    String[] colors() {
+        return new String[] {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+    }
+}
