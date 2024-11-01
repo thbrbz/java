a608215334ea62f9fdee3f4e9fef6530e3d13a68
thbrbz <thbrbz@gmail.com>
2024-10-31 22:29:42 -0300

    Feat (ResistorColorDuo): New exercise added.

---
+++ src/main/java/org/example/exercises/ResistorColorDuo.java
@@ -0,0 +1,12 @@
+package org.example.exercises;
+
+import java.util.Arrays;
+import java.util.List;
+
+class ResistorColorDuo {
+
+    int value(String[] colors) {
+        List<String> listColors = Arrays.asList("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white");
+        return listColors.indexOf(colors[0]) * 10 + listColors.indexOf(colors[1]);
+    }
+}
