3d340acf3e086262f0e2cc7ba8203375e663388d
thbrbz <thbrbz@gmail.com>
2025-01-09 00:33:12 -0300

    Refact (Acronym): Created new logic to make the code clearer.

--- src/main/java/org/example/exercises/Acronym.java
+++ src/main/java/org/example/exercises/Acronym.java
@@ -1,32 +1,22 @@
 package org.example.exercises;
 
-import java.lang.Character;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Acronym {
 
     private final String value;
 
-    Acronym(String phrase) {
-        StringBuilder sb = new StringBuilder();
-        StringBuilder result = new StringBuilder();
-
-        for (final char c : phrase.toCharArray()) {
-            if (c == ' ' && sb.toString().endsWith(" ")) {
-                continue;
-            } else if (Character.toString(c).matches("[a-zA-Z]") || c == ' ') {
-                sb.append(c);
-            } else if (c == '-' && phrase.charAt(phrase.indexOf(c)+1) != ' ') {
-                sb.append(' ');
-            }
-        }
-
-        for (String s : sb.toString().split(" "))
-            result.append(s.charAt(0));
-
-        value = result.toString().toUpperCase();
+    public Acronym(String phrase) {
+        value = Arrays.stream(phrase
+                    .replaceAll("[^a-zA-Z\\s-]", "")
+                    .split("[^a-zA-Z]"))
+                    .filter(w -> !w.isEmpty())
+                    .map(w -> String.valueOf(w.charAt(0)).toUpperCase())
+                    .collect(Collectors.joining());
     }
 
-    String get() {
+    public String get() {
         return value;
     }
 
