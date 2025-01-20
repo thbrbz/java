25814b8ab30e5035a8b3be9ee83b0629cf0f28d5
thbrbz <thbrbz@gmail.com>
2025-01-19 22:49:11 -0300

    Refact (PangramCheck): Make the code more professional.

--- src/main/java/org/example/exercises/PangramChecker.java
+++ src/main/java/org/example/exercises/PangramChecker.java
@@ -1,19 +1,12 @@
 package org.example.exercises;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class PangramChecker {
 
     public boolean isPangram(String input) {
-
-        Set<Character> letters = new HashSet<>();
-
-        for (char c : input.toLowerCase().toCharArray())
-            if (Character.isLetter(c))
-                letters.add(c);
-
-        return letters.size() == 26;
+        return input.toLowerCase().chars()
+                .filter(Character::isLetter)
+                .distinct()
+                .count() ==  26;
     }
 
 }
