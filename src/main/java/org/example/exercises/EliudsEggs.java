56d64f380c1985660069911a3c14f5cc777521a2
thbrbz <thbrbz@gmail.com>
2025-01-21 22:23:35 -0300

    Refact (EliudsEggs): Simplifying the code.

--- src/main/java/org/example/exercises/EliudsEggs.java
+++ src/main/java/org/example/exercises/EliudsEggs.java
@@ -3,20 +3,7 @@ package org.example.exercises;
 public class EliudsEggs {
 
     public int eggCount(int number) {
-
-        int[] binaryNum = new int[1000];
-        int i = 0;
-
-        while (number > 0) {
-            binaryNum[i] = number % 2;
-            number = number / 2;
-            i++;
-        }
-
-        int count = 0;
-        for (int j = i - 1; j >= 0; j--)
-            if (binaryNum[j] == 1) count += 1;
-
-        return count;
+        return Integer.bitCount(number);
     }
+
 }
