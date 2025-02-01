d26d0e6e57143a2adb75b79bb07d00454c98b3b8
thbrbz <thbrbz@gmail.com>
2025-02-01 01:07:58 -0300

    Fix (RaindropConverter): Added package to fix class compilation and refactoring the logic in case of sb equal 0.

--- src/main/java/org/example/exercises/RaindropConverter.java
+++ src/main/java/org/example/exercises/RaindropConverter.java
@@ -1,3 +1,5 @@
+package org.example.exercises;
+
 class RaindropConverter {
 
     String convert(int number) {
@@ -6,7 +8,7 @@ class RaindropConverter {
         if (number % 3 == 0) sb.append("Pling");
         if (number % 5 == 0) sb.append("Plang");
         if (number % 7 == 0) sb.append("Plong");
-        if (sb.length() == 0) sb.append(number);
+        if (sb.isEmpty()) sb.append(number);
 
         return sb.toString();
     }
